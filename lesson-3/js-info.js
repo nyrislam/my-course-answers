e6da07814899b8a8e948bb2949e51cb9d265035b
nyrislam <nyrikesheihe@gmail.com>
2025-07-12 16:21:07 +0300

    Первый коммит

---
+++ lesson-3/js-info.js
@@ -0,0 +1,14 @@
+alert("Кто разработал язык программирования JavaScript?");
+alert("Брендан Айк");
+
+alert("За сколько дней был разработан JavaScript?");
+alert("За 10 дней");
+
+alert("В каком году появился JavaScript?");
+alert("В 1995 году");
+
+alert("JavaScript — это то же самое, что и Java?");
+alert("JavaScript — это не Java");
+
+alert("Как называется спецификация JavaScript?");
+alert("ECMAScript");
