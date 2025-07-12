80671c8be6af1901a5af7d5052e7e8cc5a75b631
nyrislam <nyrikesheihe@gmail.com>
2025-07-12 18:47:36 +0300

    Initial commit

---
+++ 2. Синтаксис/Ответ на задание #3 #4/car.js
@@ -0,0 +1,26 @@
+// Название машины не меняется
+const carName = "Toyota";
+
+// Переменные, которые будут изменяться
+let maxCarSpeed = 220;
+let carOwner = "Нурислам";
+
+// Изменяем maxCarSpeed и выводим сообщение в консоль
+maxCarSpeed = 230;
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+maxCarSpeed = 240;
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+maxCarSpeed = 250;
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+// Изменяем carOwner и выводим сообщение в консоль
+carOwner = "Алексей";
+console.log('У машины изменился владелец', carOwner);
+
+carOwner = "Марина";
+console.log('У машины изменился владелец', carOwner);
+
+carOwner = "Иван";
+console.log('У машины изменился владелец', carOwner);
